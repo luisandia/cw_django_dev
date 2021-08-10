a39e7eda4b3107d6b4e7fc149c215ed9a671f54e
luisandia <luisandiamamani@gmail.com>
2021-08-10 18:24:13 -0500

    Fix port with nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.count() == 0 and User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
 
-gunicorn quizes.wsgi:application --workers=2 --bind 0.0.0.0:8000
+gunicorn quizes.wsgi:application --workers=2 --bind 0.0.0.0:8001
